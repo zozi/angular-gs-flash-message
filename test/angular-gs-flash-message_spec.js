01c24090004db7ca642545818ca35e233152f969
Gabriel Scholz <gabe.scholz@zozi.com>
2014-06-05 21:16:29 -0700

    Build

--- test/angular-gs-flash-message_spec.js
+++ test/angular-gs-flash-message_spec.js
@@ -1,22 +1,69 @@
 describe('angular-gs-flash-message', function () {
   beforeEach(module('gs.flash-message'));
 
-  var $scope;
+  var $scope, el;
 
-  beforeEach(inject(function ($rootScope) {
+  beforeEach(inject(function ($rootScope, $compile) {
     $scope = $rootScope.$new();
+    el = $compile('<div gs-flash-message="gabe"></div>')($scope);
   }));
 
-  // happy path(s)
-  it('', function () {
+  it('displays success messages on broadcast', function () {
+    var flashMessage, text = 'lel';
+
+    $scope.$apply(function () {
+      $scope.$broadcast('flash:gabe', {success: text});
+    });
+
+    flashMessage = el.children().children();
+
+    expect(flashMessage.hasClass('alert--message__success')).toBeTruthy();
+    expect(flashMessage.text()).toBe(text);
   });
 
-  // null input path
-  it('', function () {
+  it('displays error messages on broadcast', function () {
+    var flashMessage, text = 'lel';
+
+    $scope.$apply(function () {
+      $scope.$broadcast('flash:gabe', {error: text});
+    });
+
+    flashMessage = el.children().children();
+
+    expect(flashMessage.hasClass('alert--message__error')).toBeTruthy();
+    expect(flashMessage.text()).toBe(text);
   });
 
-  // wrong type path
-  it('', function () {
+  it('hides all messages on stateChangeSuccess', function () {
+    var text = 'lel';
+
+    $scope.$apply(function () {
+      $scope.$broadcast('flash:gabe', {error: text});
+      $scope.$broadcast('$stateChangeSuccess', {}, {}, {}, {});
+    });
+
+    expect(el.text()).toBe('');
+  });
+
+  it('sets messages on stateChangeSuccess', function () {
+    var flashMessage,
+      text = 'lel',
+      params = {
+        flash: {
+          gabe: {
+            success: text
+          }
+        }
+      };
+
+    $scope.$apply(function () {
+      $scope.$broadcast('$stateChangeSuccess', {}, {}, {}, params);
+    });
+
+    flashMessage = el.children().children();
+
+    expect(flashMessage.hasClass('alert--message__success')).toBeTruthy();
+    expect(el.text()).toBe('lel');
   });
 
 });
